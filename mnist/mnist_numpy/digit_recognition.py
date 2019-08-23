f68f4a7436f142e1cfb2a88b4fc20647a18f0a7f
Ankit Khandelwal <amtron521@gmail.com>
2019-08-22 21:33:34 -0400

    loading images using python-mnist and starting loss function implementation in digit_recognition.py

--- mnist/mnist_numpy/digit_recognition.py
+++ mnist/mnist_numpy/digit_recognition.py
@@ -1,28 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import gzip
 from tensorflow.examples.tutorials.mnist import input_data
+
+train_img_filepath = 'MNIST_data/train-images-idx3-ubyte.gz'
+train_labels_filepath = 'MNIST_data/train-labels-idx1-ubyte.gz'
+test_img_filepath = 'MNIST_data/t10k-images-idx3-ubyte.gz'
+test_labels_filepath = 'MNIST_data/t10k-labels-idx1-ubyte.gz'
+
+train_img, train_labels = []
+image_size = 28
 input_nodes = 784
 hidden1_layer = 512
 hidden2_layer = 256
 hidden3_layer = 128
 output_nodes = 10
 
+
 learning_rate = 1e-4
 n_iterations = 1000
 batch_size = 128
 dropout = 0.5
 
-mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
-
-n_train = mnist.train.num_exmaples
-n_validation = mnist.validation.num_examples
-n_test = mnist.text.num_exmaples
-
 weights = {}
 bias = {}
 
+
+def main():
+    set_parameters()
+    for epoch in range(n_iterations):
+        current_img, current_labels = get_next_train_batch(epoch)
+        output_results = [] # predictions
+        for img in current_img:
+            output_results.append(run_session(img))
+        calculate_cross_entropy()
+
+
+
+
 def set_parameters():
+    train_img, train_labels = get_train_img_labels()
+
     weights: {
         'w1': create_weights(input_nodes, hidden1_layer),
         'w2': create_weights(hidden1_layer, hidden2_layer),
@@ -50,11 +69,40 @@ def run_session(input_arr):
     layer_3 = np.add(np.matmul(weights['w3'], layer_2), bias['b3'])
     layer_3 = dropout(layer_3)
     output_layer = np.add(np.matmul(weights['w4'], layer_3), bias['out'])
+    return output_layer
 
 def dropout(layer):
-    num_rows, num_cols = layer.shape()
+    num_rows, num_cols = layer.shape
     for row in range(num_rows):
         for col in range(num_cols):
             if(random.random() < dropout):
                 layer[row][col] = 0
     return (1/dropout) * layer
+
+def get_train_img_labels():
+    img_file = gzip.open(train_img_filepath, 'r')
+    buffer = img_file.read(image_size * image_size * batch_size)
+    img = np.frombuffer(buffer, dtype=np.uint8).astype(np.float32)
+    img = img.reshape(batch_size, image_size, image_size, 1)
+
+    label_file = gzip.open(train_labels_filepath, 'r')
+    label = label_file.read()
+    return img, label
+
+def get_next_train_batch(epoch_num):
+    img_return, label_return = []
+    for i in range(epoch_num*batch_size, epoch_num*batch_size+batch_size, 1):
+        img_return.append(np.asarray(train_img[i]).squeeze())
+
+    for l in range(batch_size):
+        buffer = train_labels.read(1)
+        labels = np.frombuffer(buffer, dtype=np.uint8).astype(np.int64)
+        label_return.append(labels)
+
+    return img_return, label_return
+
+def calculate_cross_entropy(predictions, correct_labels):
+    
+
+
+
