532bab94733d97dd87c9b88bcc460afac5aefc7e
Ankit Khandelwal <amtron521@gmail.com>
2020-03-17 00:12:50 -0400

    added Conv2D to model

--- yolo/yolo.py
+++ yolo/yolo.py
@@ -5,7 +5,9 @@ import numpy as np
 CFG_FILENAME = "yolov3.cfg"
 
 def main():
-    pass
+    blocks = create_blocks(CFG_FILENAME)
+    print(blocks)
+    model = create_model(blocks)
 
 def create_blocks(cfg_filename):
     blocks = []
@@ -22,6 +24,19 @@ def create_blocks(cfg_filename):
             current_block[key.rstrip()] = value.lstrip()
     return blocks
 
+def create_model(blocks):
+    model = keras.Sequential()
+    for block in blocks:
+        if block["type"] == "convolutional":
+            model.add(keras.layers.Conv2D(filters=block["filters"], kernel_size=block["size"], strides=block["stride"], padding=block["pad"], activation=keras.layers.LeakyReLU(alpha=0.1)))
+        elif block["type"] == "shortcut":
+            pass
+        elif block["type"] == "upsample":
+            pass
+        elif block["type"] == "route":
+            pass
+        else: #implies yolo layer
+            pass
 
 
 
